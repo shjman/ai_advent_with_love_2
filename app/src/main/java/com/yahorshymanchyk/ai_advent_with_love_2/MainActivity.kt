235fa6e8913ddafa4c060e9508a4eab40c470783
yahor.shymanchyk <shjman1@gmail.com>
2026-03-16 18:02:42 +0100

    add new text

--- app/src/main/java/com/yahorshymanchyk/ai_advent_with_love_2/MainActivity.kt
+++ app/src/main/java/com/yahorshymanchyk/ai_advent_with_love_2/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yahorshymanchyk.ai_advent_with_love_2.ui.theme.Ai_advent_with_love_2Theme
@@ -20,10 +22,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             Ai_advent_with_love_2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        Greeting(name = "Android")
+                        Text(
+                            text = "powered by claude",
+                            modifier = Modifier
+                                .align(Alignment.BottomCenter)
+                        )
+                    }
                 }
             }
         }
